7d896235d132f034466a3454c4da01c5a6acc8d7
Faidat Egberinde <faidategberinde@gmail.com>
2026-05-05 13:17:12 +0100

    Added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="public/images/—Pngtree—chef icon illustration with hat_17265169.png" alt="chef icon" />
+      <p className="headerTitle">Chef Claude</p>
+    </header>
+  )
+}
