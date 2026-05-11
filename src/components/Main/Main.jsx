0f16a4082a1c1fc49f0d131d2f91684589f2f651
Faidat Egberinde <faidategberinde@gmail.com>
2026-05-11 11:09:48 +0100

    Added conditional and  state rendering

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,46 @@
+import React from "react"
 import './Main.css'
 
 export default function Main() {
+
+  const [ingredients, setIngredients] = React.useState([])
+
+  const ingredientsListItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+  ))
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+  }
+
   return (
     <main className="main">
-      <form>
+      <form action={addIngredient} className="addIngredientForm">
         <input 
+        type="text"
           className="ingredient" 
           aria-label="Add ingredient" 
-          name="text" 
+          name="ingredient" 
           placeholder="e.g. flour" 
         />
-        <button><span>+</span> Add ingredient</button>
+        <button className="addIngredient"><span>+</span> Add ingredient</button>
       </form>
+
+      {ingredients.length > 0 && <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredientsList">{ingredientsListItems}</ul>
+
+        <div className="recipeContainer">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button className="getRecipe">Get a recipe</button>
+        </div>
+      </section>
+}
+
     </main>
   )
 }
