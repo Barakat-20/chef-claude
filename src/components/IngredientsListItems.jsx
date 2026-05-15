103eeb0249f68e2f99b7f8127323d9cd792cc65e
Faidat Egberinde <faidategberinde@gmail.com>
2026-05-15 08:54:49 +0100

    Moved IngredientsListItems to its own compoment

---
+++ src/components/IngredientsListItems.jsx
@@ -0,0 +1,19 @@
+export default function IngredientsListItems(props) {
+  const ingredientsListItems = props.ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+  ))
+  return (
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredientsList">{ingredientsListItems}</ul>
+
+      {props.ingredients.length > 3  && <div className="recipeContainer">
+        <div>
+          <h3>Ready for a recipe?</h3>
+          <p>Generate a recipe from your list of ingredients.</p>
+        </div>
+        <button className="getRecipe">Get a recipe</button>
+      </div>}
+    </section> 
+  )
+}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,11 @@
 import React from "react"
 import './Main.css'
+import IngredientsListItems from "../IngredientsListItems.jsx"
 
 export default function Main() {
 
   const [ingredients, setIngredients] = React.useState([])
 
-  const ingredientsListItems = ingredients.map(ingredient => (
-    <li key={ingredient}>{ingredient}</li>
-  ))
 
   function addIngredient(formData) {
     const newIngredient = formData.get("ingredient")
@@ -27,19 +25,7 @@ export default function Main() {
         <button className="addIngredient"><span>+</span> Add ingredient</button>
       </form>
 
-      {ingredients.length > 0 && <section>
-        <h2>Ingredients on hand:</h2>
-        <ul className="ingredientsList">{ingredientsListItems}</ul>
-
-        {ingredients.length > 3  && <div className="recipeContainer">
-          <div>
-            <h3>Ready for a recipe?</h3>
-            <p>Generate a recipe from your list of ingredients.</p>
-          </div>
-          <button className="getRecipe">Get a recipe</button>
-        </div>}
-      </section>
-}
+      {ingredients.length > 0 && <IngredientsListItems ingredients={ingredients} />}
 
     </main>
   )
